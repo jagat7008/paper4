4cd724b9b79334ef41e5dba3e7ef46e34bd8648f
jagat7008 <94361892+jagat7008@users.noreply.github.com>
2023-03-29 15:12:18 +0530

    Update main.py

--- qus12/main.py
+++ qus12/main.py
@@ -6,7 +6,7 @@ try:
     password = 1234
     login_info = (username, password)
     login_info = login_info[0] + "," + str(login_info[1])
+    login_info("user"+1234)
     # do something with the login_info tuple
 except TypeError:
     print("error: cannot concatenate string and number")
-print(login_info("user"+1234))
